66ddac0dacbd8b69496eaf3a495edf2774499345
Vanessa Ely <vanessa.sdev@gmail.com>
2019-01-27 21:58:55 +0000

    Execise solved and successfully running

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Game from './GameDashboard';
+
+class App extends Component {
+  state = {
+    numQuestions: 0,
+    numCorrect: 0,
+  }
+  calculateScore = isCorrect => {
+    this.setState(currentState => ({
+      numQuestions: currentState.numQuestions + 1,
+      numCorrect: isCorrect ? currentState.numCorrect + 1 : currentState.numCorrect,
+    }));
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">ReactND - Coding Practice</h1>
+        </header>
+        <div className="game">
+          <h2>Mental Math</h2>
+          <Game setScore={this.calculateScore}/>
+		  <p className="text">
+          	Your Score: {this.state.numCorrect}/{this.state.numQuestions}
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/GameDashboard.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class Game extends Component {
+  constructor(props) {
+    super(props);
+    // creating state property with the object that is returned from makeNewQuestion function
+    this.state = this.makeNewQuestion();
+  }
+  makeNewQuestion = () => {
+    const values = [
+      Math.floor(Math.random() * 100),
+      Math.floor(Math.random() * 100),
+      Math.floor(Math.random() * 100),
+    ];
+    return {
+      	value1: values[0],
+    	value2: values[1],
+    	value3: values[2],
+    	proposedAnswer: Math.floor(Math.random() * 3) + (values[0] + values[1] + values[2]),
+  	};
+  };
+  updateDashboard = answer => {
+    const sum = this.state.value1 + this.state.value2 + this.state.value3;
+    // checking if the answer the user clicked is correct
+    const answerIsCorrect = answer === (sum === this.state.proposedAnswer);
+    // calling parent component method 'setScore', which is responsible for updating the
+    // user score considering it the answer was correct or not
+    this.props.setScore(answerIsCorrect);
+    // updating the state using the object returned from makeNewQuestion function
+    this.setState(currState => (this.makeNewQuestion()));
+  }
+  render() {
+    return (
+      <div>
+        <div className="equation">
+              <p className="text">{`${this.state.value1} + ${this.state.value2} + ${this.state.value3} = ${this.state.proposedAnswer}`}</p>
+        </div>
+        <button onClick={() => this.updateDashboard(true)}>True</button>
+        <button onClick={() => this.updateDashboard(false)}>False</button>
+	  </div>
+    );
+  }
+}
+
+export default Game;
